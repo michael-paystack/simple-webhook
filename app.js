f96ace3d65af1c957fa6e011251ec498dc5e8ba3
Michael Obi <michael@paystack.com>
2024-10-02 18:00:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+
+app.get("/", (request, response) => {
+  response.send("Running");
+});
+
+app.post("/hook", (request, response) => {
+  console.log(request.body);
+  response.json();
+});
